5be34f4a70fe99bf0e9c3dde9f95b98c27fcfa35
Akhileshwar Padala <Akhileshwar.Padala@toshibagcs.com>
2022-07-07 00:28:06 -0400

    java 8 level

--- NeetCode/src/main/java/org/apadala/neetcode/arraysHashing/ContainsDuplicate.java
+++ NeetCode/src/main/java/org/apadala/neetcode/arraysHashing/ContainsDuplicate.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * 217. Contains Duplicate <br>
  * https://leetcode.com/problems/contains-duplicate/<br>
- * Given an integer array nums, return true if any value appears at least twice
+ * Given an integer array numbers, return true if any value appears at least twice
  * in the array, and return false if every element is distinct.
  * 
  * 
@@ -16,6 +16,17 @@ import java.util.Set;
  */
 public class ContainsDuplicate {
 
+	/**
+	 * Hash Set has 0(1) time complexity for add , remove , contains operation ,
+	 * there are N elements to add to hashSet , if add returns false , then we hit
+	 * duplicate. <br>
+	 * total time complexity = 0(n); <br>
+	 * total space complexity = 0(n) since we are storing elements into Data
+	 * structure , DS.
+	 * 
+	 * @param nums
+	 * @return
+	 */
 	public boolean containsDuplicate(int[] nums) {
 
 		Set<Integer> numbersSet = new HashSet<>();
